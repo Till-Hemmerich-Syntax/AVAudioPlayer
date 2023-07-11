d35794f148fe4153b1bd831380363c638aa9e3da
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-07-11 11:00:51 +0200

    Initial Commit

---
+++ AVAudioPlayer/AVAudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVAudioPlayerApp.swift
+//  AVAudioPlayer
+//
+//  Created by Till Hemmerich on 11.07.23.
+//
+
+import SwiftUI
+
+@main
+struct AVAudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
